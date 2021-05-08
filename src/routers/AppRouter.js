bed746bb0152c25388bec66a29ad8265c4ad413c
rajanjamu <rajanjamu@gmail.com>
2021-05-08 15:19:52 +0530

    Router setup complete

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "../components/Header";
 import LoginPage from "../components/LoginPage";
 
 const AppRouter = () => (
   <Router>
     <div>
+      <Header />
       <Switch>
-        <Route path="/" component={LoginPage} />
+        <Route path="/" component={LoginPage} exact />
       </Switch>
     </div>
   </Router>
